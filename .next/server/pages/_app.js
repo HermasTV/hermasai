0e6f1c30791bf4d81aa8ddd10ff37d1ee3771575
HermasTV <montada7@gmail.com>
2024-05-20 01:26:17 +0300

    update gitignore

--- .next/server/pages/_app.js
+++
@@ -1,1 +0,0 @@
-(()=>{var r={};r.id=888,r.ids=[888],r.modules={3414:(r,e,t)=>{"use strict";t.r(e),t.d(e,{default:()=>u});var s=t(997);t(6764);let u=function({Component:r,pageProps:e}){return s.jsx(r,{...e})}},6764:()=>{},997:r=>{"use strict";r.exports=require("react/jsx-runtime")}};var e=require("../webpack-runtime.js");e.C(r);var t=e(e.s=3414);module.exports=t})();
